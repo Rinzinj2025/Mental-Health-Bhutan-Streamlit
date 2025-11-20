f15089820c1523bff5066424170a34970b788801
Rinzin <rinzinj2025@gmail.com>
2025-11-20 15:58:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import sklearn as sk
 import matplotlib.pyplot as plt
 import seaborn as sns
 
