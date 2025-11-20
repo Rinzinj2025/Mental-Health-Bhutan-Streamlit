340e22ad65812708a7b6ff848834b082a47e1ec8
Rinzin <rinzinj2025@gmail.com>
2025-11-20 16:03:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 st.title("Mental Health Stress Level Predictor with Visualizations (Bhutan)")
